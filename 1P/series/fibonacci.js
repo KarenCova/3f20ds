f3f6ab0abc57ab837ed8e1815a10a9b9eaac4486
KarenCova <72278804+KarenCova@users.noreply.github.com>
2020-11-07 14:36:45 -0600

    Update fibonacci.js

--- 1P/series/fibonacci.js
+++ 1P/series/fibonacci.js
@@ -1,1 +1,11 @@
-
+// Serie de Fibonacci
+function fibonacci(num){
+      let a = 1, b = [0,1], guardar;
+  while (num >= 0){
+      guardar = a;
+      a = a + b[num];
+      num--;
+  }
+  return b;
+}
+console.log(fibonacci(10));
