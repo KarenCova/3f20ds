b46035bae3de3b179df96abf7e75c268b9097e41
KarenCova <72278804+KarenCova@users.noreply.github.com>
2020-11-15 01:55:28 -0600

    Update primos.js

--- 1P/series/primos.js
+++ 1P/series/primos.js
@@ -1,22 +1,18 @@
 //Números primos 
-let n1 = 1;
-let n2 = 0;
-let k = 0;
-var x = 10;
+let i = 1, p = 2, esP = 1;
 
-while (n1 <= x){
-  while (n2 < n1){
-    if (n1 % n2 == 0 && n2 != 1 n2 != n1){
-    k = 1;
+while (i <= 10){
+    let m = Math.round(p / 2);
+    while (m >= 2 && esP == 1){
+        if (p % m == 0){
+            esP = 0;    
+        }
+        m--;    
     }
-    n2++;
-  }
-  if (k == 0){
-  console.log(n2);
-  }
-  else{
-  k = 0;
-  }
-  n1++;
-  n2 = 0;
+    if(esP == 1){
+        console.log(p);
+        i++;
+    } 
+    p += 1;
+    esP = 1;
 }
