208d3fcf7501746ec1c54dafa69fe282f5c4c328
KarenCova <72278804+KarenCova@users.noreply.github.com>
2020-11-17 13:54:52 -0600

    Update Ln2.js

--- 1P/consola/Ln2.js
+++ 1P/consola/Ln2.js
@@ -1,22 +1,9 @@
-ln = process.openStdin();
-console.log("Introduce el tamaño de la serie:")
-ln.addListener("data",function(n){
-    har(n);
-    process.exit()
-})
-function har(x) {
-    if (x == 1){
-        console.log("1");
-    }
-   else {
-       console.log("1")
-    for(k = 2; k <= x ; k++) {
-        if (k % 2 == 0) {
-            console.log("- 1/" +k);
-        }
-        else if (k % 2 != 0) {
-            console.log("1/" +k);
-        }
-    }
- }
+let ln = 0, l = 0;
+let k = prompt('Valor para "x": ');
+let n = prompt('Introduce el número de iteraciones: ');
+
+for( i = 1; i <= n; i++){
+  l = (Math.pow(k, i) * Math.pow(-1, i + 1))/i;
+  ln = l;
+  console.log(ln);
 }
