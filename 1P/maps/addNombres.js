75042f5143254148d023e4a1ca7b6c230ea80434
KarenCova <72278804+KarenCova@users.noreply.github.com>
2020-11-18 02:19:34 -0600

    Delete addNombres.js

--- 1P/maps/addNombres.js
+++
@@ -1,9 +0,0 @@
-let nomMap = new Map(), Base = "Nombre", num = 1;
-while(nomMap.size < 10){
-    let nomC = (Base + num).toString();
-    nomMap.set(num, nomC);
-    num++
-}
-for(let Nombre of nomMap.values()){
-    console.log(Nombre);
-}
