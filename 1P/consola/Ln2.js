d77e2919963f3d3c05b3331767e391911de17658
KarenCova <72278804+KarenCova@users.noreply.github.com>
2020-11-18 02:04:00 -0600

    Delete Ln2.js

--- 1P/consola/Ln2.js
+++
@@ -1,9 +0,0 @@
-let ln = 0, l = 0;
-let k = prompt('Valor para "x": ');
-let n = prompt('Introduce el número de iteraciones: ');
-
-for( i = 1; i <= n; i++){
-  l = (Math.pow(k, i) * Math.pow(-1, i + 1))/i;
-  ln = l;
-  console.log(ln);
-}
