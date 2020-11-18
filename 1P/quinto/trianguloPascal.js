4bd59ac1c5f084dd0bd5689a885ebb1967983da7
KarenCova <72278804+KarenCova@users.noreply.github.com>
2020-11-18 02:12:39 -0600

    Delete trianguloPascal.js

--- 1P/quinto/trianguloPascal.js
+++
@@ -1,9 +0,0 @@
-function Pascal(n) {
-    r =[[1], [1, 1]];
-    for (let i=3; i <= n; i++) {
-        r.push(
-            [...Array(i)].map((el, l, x) =>
-                l==0 || l == x.length -1 ? 1 : r[i-2][l-1]+r [i-2][l]));
-    }
-    return r;
-} console.log(Pascal(7));
