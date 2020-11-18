bbf4b567ba089ee22bd6822157ba4a22362b3423
KarenCova <72278804+KarenCova@users.noreply.github.com>
2020-11-18 02:14:35 -0600

    Delete lunes1.js

--- 1P/modular/lunes1.js
+++
@@ -1,8 +0,0 @@
-let dia = 0,
-    sem = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'], dTotal, mm;
-    function calcular() {
-        dTotal = 11 + dia;
-        mm = dTotal % 7;
-        return sem[mm];
-    }
-    console.log('Dentro de 11 días será: ', calcular());
